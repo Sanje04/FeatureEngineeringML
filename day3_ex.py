0f49760013499a22d2d5efb2bb95a8b1a5c2405e
Sanje Divakaran <sanje.divakaran@gmail.com>
2025-07-10 11:46:53 -0400

    Additional Comments

--- day3_ex.py
+++ day3_ex.py
@@ -47,6 +47,7 @@ y = df_one_hot['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # train logistic regression model
+# finding the best weights for each column
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
